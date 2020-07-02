23af44cff819acc53a15385a18b39beaef7f5ff8
masatoshi.emori <masaemori0513@gmail.com>
2020-07-02 10:53:47 +0000

    twitterクローン課題②

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
+      # topページから繋がる全てのページにインスタンス変数で連携
       @micropost = current_user.microposts.build  # form_with 用
       @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
+  has_many :reverses_of_favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favorite_from, through: :reverses_of_favorites, source: :user
+
     validates :content, presence: true, length:{maximum: 255}
 end
